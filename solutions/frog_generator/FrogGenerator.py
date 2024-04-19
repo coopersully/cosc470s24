81776cba6867fe8509c3391c79c3c318453539ca
Cooper Sullivan <thecoopersully@gmail.com>
2024-04-19 17:17:56 -0500

    Update FrogGenerator.py

--- solutions/frog_generator/FrogGenerator.py
+++ solutions/frog_generator/FrogGenerator.py
@@ -23,16 +23,16 @@ def define_discriminator(in_shape=(32, 32, 3)):
     model = Sequential()
     # normal
     model.add(Conv2D(64, (3, 3), padding='same', input_shape=in_shape))
-    model.add(LeakyReLU(alpha=0.2))
+    model.add(LeakyReLU(negative_slope=0.2))
     # downsample
     model.add(Conv2D(128, (3, 3), strides=(2, 2), padding='same'))
-    model.add(LeakyReLU(alpha=0.2))
+    model.add(LeakyReLU(negative_slope=0.2))
     # downsample
     model.add(Conv2D(128, (3, 3), strides=(2, 2), padding='same'))
-    model.add(LeakyReLU(alpha=0.2))
+    model.add(LeakyReLU(negative_slope=0.2))
     # downsample
     model.add(Conv2D(256, (3, 3), strides=(2, 2), padding='same'))
-    model.add(LeakyReLU(alpha=0.2))
+    model.add(LeakyReLU(negative_slope=0.2))
     # classifier
     model.add(Flatten())
     model.add(Dropout(0.4))
@@ -49,32 +49,32 @@ def define_generator(latent_dim):
     # foundation for 4x4 image
     n_nodes = 256 * 4 * 4
     model.add(Dense(n_nodes, input_dim=latent_dim))
-    model.add(LeakyReLU(alpha=0.2))
+    model.add(LeakyReLU(negative_slope=0.2))
     model.add(Reshape((4, 4, 256)))
     # upsample to 8x8
     model.add(Conv2DTranspose(128, (4, 4), strides=(2, 2), padding='same'))
-    model.add(LeakyReLU(alpha=0.2))
+    model.add(LeakyReLU(negative_slope=0.2))
     # upsample to 16x16
     model.add(Conv2DTranspose(128, (4, 4), strides=(2, 2), padding='same'))
-    model.add(LeakyReLU(alpha=0.2))
+    model.add(LeakyReLU(negative_slope=0.2))
     # upsample to 32x32
     model.add(Conv2DTranspose(128, (4, 4), strides=(2, 2), padding='same'))
-    model.add(LeakyReLU(alpha=0.2))
+    model.add(LeakyReLU(negative_slope=0.2))
     # output layer
     model.add(Conv2D(3, (3, 3), activation='tanh', padding='same'))
     return model
 
 
 # define the combined generator and discriminator model, for updating the generator
-def define_gan(g_model, d_model):
+def define_gan(generator, discriminator):
     # make weights in the discriminator not trainable
-    d_model.trainable = False
+    discriminator.trainable = False
     # connect them
     model = Sequential()
     # add generator
-    model.add(g_model)
+    model.add(generator)
     # add the discriminator
-    model.add(d_model)
+    model.add(discriminator)
     # compile model
     opt = Adam(learning_rate=0.0002, beta_1=0.5)
     model.compile(loss='binary_crossentropy', optimizer=opt)
@@ -156,7 +156,7 @@ def summarize_performance(epoch, g_model, d_model, dataset, latent_dim, n_sample
 
 
 # train the generator and discriminator
-def train(g_model, d_model, gan_model, dataset, latent_dim, n_epochs=200, n_batch=64):
+def train(g_model, d_model, gan_model, dataset, latent_dim, n_epochs=200, n_batch=32):
     bat_per_epo = int(dataset.shape[0] / n_batch)
     half_batch = int(n_batch / 2)
     # manually enumerate epochs
